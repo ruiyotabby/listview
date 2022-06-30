ed5921f86e371cc99d2b0f440278dc284a4d5fb6
Ruiyot Abby <ruiyotabby@gmail.com>
2022-06-30 13:56:06 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,6 @@ void main() {
               child: ListView(
                 padding: EdgeInsets.all(8),
                 children: [
-                  Text(
-                    'Tupo Site',
-                    style: TextStyle(fontSize: 20, color: Colors.blue),
-                  ),
-                  Text(
-                    'Tupo Site',
-                    style: TextStyle(fontSize: 20, color: Colors.blue),
-                  ),
-                  Text(
-                    'Tupo Site',
-                    style: TextStyle(fontSize: 20, color: Colors.blue),
-                  ),
                 ],
               ),
             ),
