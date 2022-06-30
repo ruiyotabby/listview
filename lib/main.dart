ed435eb51a9b86e955cce785e4f712d67f3fc5d1
Ruiyot Abby <ruiyotabby@gmail.com>
2022-06-30 13:14:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
           children: [
             Container(
               height: 300,
-              child: ,
+              child: ListView(),
             ),
           ],
         ),
