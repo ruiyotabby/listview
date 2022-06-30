9da9528c9c99d128f60bc6393d303f9f19a63a66
Ruiyot Abby <ruiyotabby@gmail.com>
2022-06-30 12:57:19 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(appBar: AppBar(title: Text('Listview'),),),),);
+  runApp(MaterialApp(home: Scaffold(appBar: AppBar(title: Text('Listview in'),),),),);
 }
